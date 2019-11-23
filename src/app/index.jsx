406e6dddb6dde308fa7a490be317712fb7e1c4d8
codemachin <viveksome@gmail.com>
2019-11-23 23:37:57 +0530

    first react app

---
+++ src/app/index.jsx
@@ -0,0 +1,15 @@
+// console.log("hello world")
+
+// import { store } from "./store";
+
+// console.log(store.getState())
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components/Main";
+// import { Dashboard } from "./components/Dashboard";
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("app")
+)
